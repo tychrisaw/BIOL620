b700b857ede80a354f8a196df001309a563b571f
tychrisaw <98413828+tychrisaw@users.noreply.github.com>
2022-02-07 22:34:37 -0500

    Add files via upload

---
+++ Assignment 1-TW.R
@@ -0,0 +1,38 @@
+GSM4843:  Homo sapiens:  GDS223
+GSE ID:  344
+Platform ID:  GPL830
+
+GSM4844:  Homo sapiens:  GDS223
+GSE ID:  344
+Platform ID:  GPL830
+
+GSM4845: Homo sapiens:  GDS223
+GSE ID:  344
+Platform ID:  GPL830
+
+GSM4846:  Homo sapiens:  GDS223
+GSE ID:  344
+Platform ID:  GPL830
+GSM4847:  Homo sapiens:  GDS223
+GSE ID:  344
+Platform ID:  GPL830
+
+GSM1536990:  Homo sapiens:  GDS5913
+GSE ID:  62947
+Platform ID:  GPL570
+
+GSM1536991:  Homo sapiens:  GDS5913
+GSE ID:  62947
+Platform ID:  GPL570
+
+GSM1536992  Homo sapiens:  GDS5913
+GSE ID:  62947
+Platform ID:  GPL570
+
+GSM1536993:  Homo sapiens:  GDS5913
+GSE ID:  62947
+Platform ID:  GPL570
+
+GSM1536994:  Homo sapiens:  GDS5913
+GSE ID:  62947
+Platform ID:  GPL570
